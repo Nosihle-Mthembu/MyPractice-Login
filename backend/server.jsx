8fca4f9587c7257698fb7f18c079a5f736776a0a
Nosihle Mthembu <bongekam16@gmail.com>
2024-09-03 13:50:20 +0200

    login

--- backend/server.jsx
+++ backend/server.jsx
@@ -3,9 +3,9 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
